0f3c414f6abb7d7e41990ac6191d3ec3d4dd0464
Tien Hoang Pham <tienhoang.pham@bc.fi>
2021-02-19 12:48:53 +0200

    Update popup card draw decision for RealPlayer

--- src/classes/RealPlayer.ts
+++ src/classes/RealPlayer.ts
@@ -3,6 +3,6 @@ import { Card } from "./Card";
 
 export class RealPlayer extends AbstractPlayer {
     requestCard(): boolean {
-        return this.getSum() < 21;
+        return confirm(`Your sum is currently ${this.getSum()}. Would you like to request another card?`);
     }
 }
